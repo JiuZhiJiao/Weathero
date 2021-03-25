6f3d10af387b16319c2d5019fba0f5555d9f4138
JiuZhiJiao <jiuzhijiao@gmail.com>
2021-03-25 14:53:29 +1100

    Create the project
    created the project named Weathero

---
+++ Weathero/Weathero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathero
+//
+//  Created by JiuZhiJiao on 25/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
