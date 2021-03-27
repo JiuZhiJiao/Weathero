72e30ea1bc6da3c9218526f1ac67da4b75280060
JiuZhiJiao <jiuzhijiao@gmail.com>
2021-03-27 20:25:25 +1100

    Create table view & set icon
    created the table view on quiz page, and set the icon of applicaiton

--- Weathero/Weathero/QuizViewController.swift
+++ Weathero/Weathero/QuizViewController.swift
@@ -7,14 +7,61 @@
 
 import UIKit
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var ScenarioTableView: UITableView!
+    
+    struct Scenario {
+        var image: UIImage?
+        var name: String
+    }
+    // Default date
+    var scenarios = [
+        Scenario(image: #imageLiteral(resourceName: "ClimateChange"), name: "Climate Change"),
+        Scenario(image: #imageLiteral(resourceName: "Droughts"), name: "Droughts"),
+        Scenario(image: #imageLiteral(resourceName: "Floods"), name: "Floods")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Change the frame of navigation bar
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Table View
+        ScenarioTableView.delegate = self
+        ScenarioTableView.dataSource = self
+        ScenarioTableView.backgroundColor = .white
+        ScenarioTableView.reloadData()
     }
     
+    // MARK: - Table View Function
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scenarios.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "scenarioCell", for: indexPath) as! ScenarioTableViewCell
+        
+        cell.scenarioImage?.image = scenarios[indexPath.row].image
+        cell.scenarioImage?.layer.cornerRadius = 10.0
+        //cell.scenarioImage?.clipsToBounds = true
+        cell.scenarioImage?.contentMode = .scaleAspectFill
+        
+        cell.scenarioName?.text = scenarios[indexPath.row].name
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
 
     /*
     // MARK: - Navigation
