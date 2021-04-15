fb6873070b46ab33bdb661050fc6d5d4cb5f44c3
JiuZhiJiao <jiuzhijiao@gmail.com>
2021-04-16 00:47:44 +1000

    Update Home Page
    Use the state variable to jump from home page to quiz page

--- Iteration 2/Weathero/Weathero/View/ContentView.swift
+++ Iteration 2/Weathero/Weathero/View/ContentView.swift
@@ -8,18 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 0
     var body: some View {
-        TabView {
-            HomeView()
+        TabView(selection: $selection) {
+            HomeView(selection: $selection)
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
+                .tag(0)
             QuizView()
                 .tabItem {
                     Image(systemName: "text.book.closed")
                     Text("Quiz")
                 }
+                .tag(1)
             TestView()
                 .tabItem {
                     Image(systemName: "questionmark.circle")
--- Iteration 2/Weathero/Weathero/View/HomeViewGroup/HomeView.swift
+++ Iteration 2/Weathero/Weathero/View/HomeViewGroup/HomeView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
+    @Binding var selection: Int
     var body: some View {
         NavigationView {
             ScrollView(.vertical) {
@@ -26,18 +27,23 @@ struct HomeView: View {
                 .padding(.init(top: 16, leading: 32, bottom: 0, trailing: 32))
                 
                 VStack{
-                    Text("Press the Quiz Button to help me")
-                        .fontWeight(.bold)
-                        .font(.system(.title2, design: .rounded))
-                        .foregroundColor(.white)
-                        .frame(width: 350, height: 50)
-                        .background(Rectangle().fill(Color.blue))
-                        .cornerRadius(10)
-                        .shadow(radius: 3)
+                    Button(action: {self.selection = 1}) {
+                        Text("Help Me")
+                            .font(.system(.title2, design: .rounded))
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 50)
+                            .background(Rectangle().fill(Color.blue))
+                            .cornerRadius(10)
+                            .shadow(radius: 3)
+                    }
+                    
                     Spacer()
                         .frame(width: 100, height: 20)
                 }
                 .padding(.top)
+                
+                
                 
             }
             .navigationTitle("Welcome")
@@ -46,12 +52,11 @@ struct HomeView: View {
 }
 
 
-
 // preview of current view
 #if DEBUG
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        HomeView(selection: .constant(0))
     }
 }
 #endif
